3e0c5b15ff62b97796273f0e0b1ca6fb8ff4ed77
Marko <marko.uksanovic1@gmail.com>
2024-01-07 15:39:30 +0000

    removed rustlings to move to its own repo

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let num = "ten";
+    let result: i32 = num.parse().unwrap(); // Error: trying to parse a string to i32
+
+    let mut vector = vec![1, 2, 3];
+    println!("Fourth element: {}", vector[3]); // Error: out-of-bounds access
+
+    let unused_variable = 42; // Warning: unused variable
 }
